75f1c44b877169a6b5f742754ea8f3e7b431dd7d
Chad Zhuang <changda0616@gmail.com>
2017-12-13 14:45:35 +0800

    Add sparklines and google map api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const API_KEY = 'bbe12933c75c349a33520b27eb60d35a';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export default function fetchWeather(city){
+export default function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`
     const request = axios.get(url);
     return {
-        type : FETCH_WEATHER,
+        type: FETCH_WEATHER,
         payload: request
     }
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,32 @@
-import React,{ Component } from 'react';
-import { connect } from "react-redux";
-
-export class WeatherList extends Component{
-
-    renderWeather(cityData){
-        return(
+import React, { Component } from 'react'
+import { connect } from "react-redux"
+import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
+export class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name
+        const temps = cityData.list.map(weather => weather.main.temp)
+        const Pressures = cityData.list.map(weather => weather.main.pressure)
+        const humidities = cityData.list.map(weather => weather.main.humidity)
+        const { lon, lat } = cityData.city.coord;
+        return (
             <tr key={cityData.city.id}>
-                <td>{cityData.city.name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
+                <td>
+                    <Chart data={temps} color="orange" unit="K" />
+                </td>
+                <td>
+                    <Chart data={Pressures} color="gray" unit="hPA" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="blue" unit="%" />
+                </td>
             </tr>
         )
     }
-    render(){
-        return(
-            <table className ='table table-hover'>
+    render() {
+        return (
+            <table className='table table-hover'>
                 <thead>
                     <tr>
                         <th>City</th>
@@ -29,7 +43,7 @@ export class WeatherList extends Component{
     }
 }
 
-function mapStateToProps({ weather }){
+function mapStateToProps({ weather }) {
     return { weather }
 }
 // equal to above
