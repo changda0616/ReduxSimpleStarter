9f4216947f53323ca428b38412949abca00b1ec4
Chad Zhuang <changda0616@gmail.com>
2017-12-13 17:19:26 +0800

    Route Definition

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import App from './components/app'
+import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends Component {
+  render () {
+    return (
+      <div>hello!</div>
+    )
+  }
+}
+class GoodBye extends Component {
+  render () {
+    return (
+      <div>GoodBye!</div>
+    )
+  }
+}
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={Hello} />
+        <Route path='/bye' component={GoodBye} />
+      </div>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
